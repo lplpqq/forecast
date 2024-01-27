476edefbe05c1317b92fbb6e10914c7439317c83
lplpqq <69588388+lplpqq@users.noreply.github.com>
2024-01-27 19:27:46 +0100

    Add files via upload

--- forecast/services/openweathermap.py
+++ forecast/services/openweathermap.py
@@ -8,25 +8,30 @@ from models import Granularity
 from .base import Provider
 
 
-class WeatherBit(Provider):
+class OpenWeatherMap(Provider):
     def __init__(self, api_key: str, session: ClientSession):
-        super(WeatherBit, self).__init__(api_key, 'https://api.weatherbit.io/v2.0', session)
+        super(OpenWeatherMap, self).__init__(api_key,
+                                             'https://history.openweathermap.org/data/2.5',
+                                             session)
 
-    async def get_historical_weather(self, granularity: Literal['hourly', 'daily'],
+    async def get_historical_weather(self, granularity: Granularity,
                                      coordinate: Coordinate,
                                      start_date: datetime, end_date: datetime):
         if granularity is Granularity.HOUR:
-            granularity = 'hourly'
+            type_ = 'hour'
         elif granularity is Granularity.DAY:
-            granularity = 'daily'
+            type_ = 'day'
+        else:
+            raise ValueError(...)
 
         return await self._get(
-            f'/history/{granularity}',
+            f'/history/city',
             params={
                 'lat': coordinate.latitude,
                 'lon': coordinate.longitude,
-                'start_date': start_date.strftime("%Y-%m-%d"),
-                'end_date': end_date.strftime("%Y-%m-%d"),
-                'key': self.api_key,
+                'type': type_,
+                'start_date': int(datetime.timestamp(start_date)),
+                'end_date': int(datetime.timestamp(end_date)),
+                'appid': self.api_key,
             }
         )
